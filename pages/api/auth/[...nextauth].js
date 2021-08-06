08da7cb5e4f51ba08e98339d7f7f70ab3c00590b
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-07 01:53:36 +0700

    feat: add basic auth using email magic link

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+const options = {
+  site: process.env.NEXTAUTH_URL,
+  providers: [
+    Providers.Email({
+      server: {
+        port: 465,
+        host: process.env.EMAIL_HOST,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  database: {
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  session: {
+    jwt: true,
+    maxAge: 24 * 60 * 60
+  },
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req, res) => NextAuth(req, res, options);
