bcd011cdddb2d5a7f09e72c7c15d386283eeef35
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-07 19:55:07 +0700

    fix: delete port number on database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -23,7 +23,6 @@ const options = {
   database: {
     type: 'mysql',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
